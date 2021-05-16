2d7db26a0bfd5ed4234a4344f451c6c4bdfd8e5b
vinaybajjuri58 <vinaybajjuri58@gmail.com>
2021-05-16 22:40:55 +0530

    Test:Added tests for reducer function

--- src/Context/DataContext/DataProvider.js
+++ src/Context/DataContext/DataProvider.js
@@ -2,25 +2,26 @@ import { useContext, useReducer, useState } from "react";
 import { DataContext } from "./DataContext";
 import { ReducerFunction } from "./ReducerFunction";
 import { data } from "./Data";
+export const initialState = {
+  videos: data,
+  categories: [],
+  playlists: [
+    {
+      id: 123,
+      name: "Watch Later",
+      videos: ["8egWflM1xok"],
+    },
+    {
+      id: 124,
+      name: "Wealth creation",
+      videos: [],
+    },
+  ],
+  liked: [],
+  history: [],
+};
 export const DataProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(ReducerFunction, {
-    videos: data,
-    categories: [],
-    playlists: [
-      {
-        id: 123,
-        name: "Watch Later",
-        videos: ["8egWflM1xok"],
-      },
-      {
-        id: 124,
-        name: "Wealth creation",
-        videos: [],
-      },
-    ],
-    liked: [],
-    history: [],
-  });
+  const [state, dispatch] = useReducer(ReducerFunction, initialState);
   const [snackBarContent, setSnackBarContent] = useState("");
   const [displaySnackBar, setDisplaySnackBar] = useState(false);
   const removeSnackBar = () => {
